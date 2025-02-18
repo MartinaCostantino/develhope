c17afbc28e67a021c5ed7b31d8c1bfc372177cc1
Martina <martinacostantino@yahoo.it>
2025-02-18 13:05:53 +0100

    es_dom1

---
+++ esercizio_navigareDOM/dom1.js
@@ -0,0 +1,12 @@
+document
+const html = document.documentElement
+const body = html.children[1]
+const ul = body.children[0]
+ul.parentElement
+ul.children[1]
+const liDue =ul.children[1]
+liDue.nextElementSibling
+liDue.previousElementSibling
+document.innerText
+
+
