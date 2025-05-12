2336ea092db42c447041731f8593d21a0504c1c9
Martina <martinacostantino@yahoo.it>
2025-05-12 18:51:27 +0200

    es4

--- node3/server.mjs
+++ node3/server.mjs
@@ -3,10 +3,14 @@ import { createServer } from 'node:http';
 const server = createServer((req, res) => {
     console.log('Request received');
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Node.js Server</h1></body></html>');    
+    res.setHeader('Content-Type', 'text/application/json');
+
+    const jsonResponse = JSON.stringify({'Location': 'Mars'});
+    res.end(jsonResponse);    
 });
 
 server.listen(3000, () => {
     console.log('Server running at http://localhost:3000/');
 });
+
+// content-length:19
